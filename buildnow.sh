1555f6060fa864b1cb62b4d0d0a7327d03d0b5a7
kx1t <kx1t@amsat.org>
2026-09-23 17:43:47 -0400

    Bring in aiscatcher/buildnow.sh updates that were mistakenly committed to main (3a660c4, cc61028)

--- buildnow.sh
+++ buildnow.sh
@@ -5,7 +5,7 @@ echo "Shell: $(bash --version)"
 
 [[ "$1" != "" ]] && BRANCH="$1" || BRANCH="$(git branch --show-current)"
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
-[[ "$ARCHS" == "" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
+[[ "$ARCHS" == "" ]] && ARCHS="linux/arm64,linux/amd64"
 
 BASETARGET1=ghcr.io/sdr-enthusiasts
 BASETARGET2=ghcr.io/sdr-enthusiasts
